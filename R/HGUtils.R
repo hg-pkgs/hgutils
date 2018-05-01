b8f6433b712fe4007151b7b10a9925ddb86d1975
Héctor van den Boorn <hvdboorn@gmail.com>
2018-05-02 00:07:39 +0200

    q

--- R/HGUtils.R
+++ R/HGUtils.R
@@ -4,6 +4,10 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+devtools::use_package("dplyr","Depends")
+devtools::use_package("numbers","Depends")
+devtools::use_package("magrittr","Depends")
+
 #' Installs and loads specified packages
 #'
 #' @param packages A character list of package names
@@ -12,6 +16,7 @@
 #' @return NULL
 #'
 #' @examples install_load_packages(c("ggplot2","dplyr"))
+#' @export
 install_load_packages = function(packages,install = TRUE){
   for(package in packages){
     if(!require(package, character.only = TRUE) & install){
@@ -20,7 +25,19 @@ install_load_packages = function(packages,install = TRUE){
     }
   }
 }
-install_load_packages(c("devtools","ggplot2","dplyr","numbers","roxygen2"))
+
+#' Loads (and optionally install) commonly used packages
+#'
+#' @param install Whether to install a package if missing
+#'
+#' @return NULL
+#'
+#' @export
+load_common_packages = function(install = T){
+install_load_packages(c("numbers","magrittr","colorspace","RColorBrewer","grid","gridExtra","readxl","writexl","shinydashboard","shinyBS",
+                        "shiny","shinyjs","ggplot2","shinyWidgets","shinydashboard","shinyBS","shiny","shinyjs","rms","devtools",
+                        "ggthemes","stringr","reshape2","gridGraphics","tidyverse"),install = install)
+}
 
 #' Clears the workspace and sets the working directory automatically to the Dropbox folder
 #'
@@ -29,6 +46,7 @@ install_load_packages(c("devtools","ggplot2","dplyr","numbers","roxygen2"))
 #' @return NULL
 #'
 #' @examples startup("source_webinterface/")
+#' @export
 startup = function(folder = "source_webinterface/"){
   rm(list = ls(pos = .GlobalEnv), envir = .GlobalEnv)
   gc()
@@ -43,13 +61,14 @@ startup = function(folder = "source_webinterface/"){
 #'
 #' @param limits The limits of the axis. May be a list of 2 elements with lower and upper bounds, or a
 #'               single digit (the upperbound, the lowerbound is then assumed to be 0).
-#' @param N The `unit` stepsize; The eventual interval size will be multiples of the divisors of N.
+#' @param N The unit stepsize; The eventual interval size will be multiples of the divisors of N.
 #' @param max_breaks Maximum amount of steps
 #' @param int_only Whether only integer divisors of N may be used for interval sizes
 #'
-#' @return A list of maximum `max_breaks` elements with break elements.
+#' @return A list of maximum max_breaks elements with break elements.
 #'
 #' @examples get_breaks(24, N=12, max_breaks=15)
+#' @export
 get_breaks = function(limits, N=10, max_breaks=10, int_only=TRUE) #checken van 181-600, N=12, 10 max
 {
   if(length(limits)==1) {xmin=0;xmax=limits} else {xmin=limits[1]; xmax=limits[2]}
@@ -65,9 +84,10 @@ get_breaks = function(limits, N=10, max_breaks=10, int_only=TRUE) #checken van 1
 #'
 #' @param N Number of objects
 #'
-#' @return A named list with elements `rows` and `columns` specifying the size of the optimal grid.
+#' @return A named list with elements rows and columns specifying the size of the optimal grid.
 #'
 #' @examples get_cr(5)
+#' @export
 get_cr = function(N) {N %>% sqrt %>% ceiling %>% c((N/.) %>% ceiling, .) %>% setNames(c("rows","columns"))}
 
 #' Removes any NA from a list
@@ -77,6 +97,7 @@ get_cr = function(N) {N %>% sqrt %>% ceiling %>% c((N/.) %>% ceiling, .) %>% set
 #' @return A list without NA elements
 #'
 #' @examples rmNA(c(1,NA,5,6))
+#' @export
 rmNA = function(LIST) {LIST[!is.na(LIST)]}
 
 #' Rounds a number to a specified amount of digits and returns the string value
@@ -87,4 +108,5 @@ rmNA = function(LIST) {LIST[!is.na(LIST)]}
 #' @return A string value of the number rounded to the specified amount of digits.
 #'
 #' @examples rndDbl(1.26564,digits = 2)
+#' @export
 rndDbl = function(dbl, digits=3) {sprintf(paste0("%.",digits,"f"),round(dbl, digits))}
